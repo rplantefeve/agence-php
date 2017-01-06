cef2266cfc9ee6c653437396d034e582df839f4e
Lucas <hennaertl@gmail.com>
2017-01-06 11:22:35 +0100

    FixBug /default instead of /defaut

--- src/RocketfireAgenceMainBundle/Controller/DefaultController.php
+++ src/RocketfireAgenceMainBundle/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/defaut")
+     * @Route("/default")
      */
     public function indexAction()
     {
