ba5244c592e9efb3bdd7ddc83976a76d0c58d5df
samuel michaux <kameleon836@gmail.com>
2017-01-10 13:40:43 +0100

    Correct Bug - user symfony\method

--- src/RocketfireAgenceMainBundle/Controller/HomeController.php
+++ src/RocketfireAgenceMainBundle/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace RocketfireAgenceMainBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 /**
